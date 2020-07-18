87ee484df9e15737cef448090b7b3419b7bec20b
Ajith P Mohan <ajithpmohan90@gmail.com>
2020-07-18 23:29:45 +0530

    test (cypress): test case to check whether logged in user can visit sign in page or not

--- cypress/integration/authentication.spec.js
+++ cypress/integration/authentication.spec.js
@@ -1,5 +1,29 @@
 describe('Authentication', () => {
-  it('Can sign in as admin', () => {
+  it('Can sign up', () => {
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: 'http://0.0.0.0:9000/api/v1/account/sign-up/',
+      response: {
+        message:
+          'Account activation email sent to ajithpmohan@example.com',
+      },
+    });
+
+    cy.visit('/account/signup');
+
+    cy.get('input#email').type('ajithpmohan@example.com');
+    cy.get('input#firstName').type('Ajith');
+    cy.get('input#lastName').type('P Mohan');
+    cy.get('input#password').type('abc12345', { log: false });
+    cy.get('input#password2').type('abc12345', { log: false });
+    cy.get('select#groups').select('RIDER');
+    cy.get('button').contains('Sign Up').click();
+
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('Can log in as admin', () => {
     cy.server();
     cy.route({
       method: 'POST',
@@ -25,7 +49,7 @@ describe('Authentication', () => {
     cy.location('pathname').should('eq', '/');
   });
 
-  it('Can sign in as driver', () => {
+  it('Can log in as driver', () => {
     cy.server();
     cy.route({
       method: 'POST',
@@ -51,7 +75,7 @@ describe('Authentication', () => {
     cy.location('pathname').should('eq', '/driver');
   });
 
-  it('Can sign in as rider', () => {
+  it('Can log in as rider', () => {
     cy.server();
     cy.route({
       method: 'POST',
@@ -61,7 +85,7 @@ describe('Authentication', () => {
         access: 'ACCESS_TOKEN',
         user: {
           email: 'rider@example.com',
-          fullname: 'Rider man',
+          fullname: 'Rider',
           avatar: null,
           type: 'RIDER',
         },
@@ -77,27 +101,33 @@ describe('Authentication', () => {
     cy.location('pathname').should('eq', '/rider');
   });
 
-  it('Can sign up', () => {
+  it('Cannot visit the login page when logged in', () => {
     cy.server();
     cy.route({
       method: 'POST',
-      url: 'http://0.0.0.0:9000/api/v1/account/sign-up/',
+      url: 'http://0.0.0.0:9000/api/v1/account/token/',
       response: {
-        message:
-          'Account activation email sent to ajithpmohan@example.com',
+        refresh: 'REFRESH_TOKEN',
+        access: 'ACCESS_TOKEN',
+        user: {
+          email: 'rider@example.com',
+          fullname: 'Rider',
+          avatar: null,
+          type: 'RIDER',
+        },
       },
     });
 
-    cy.visit('/account/signup');
+    cy.visit('/account/signin');
+    cy.location('pathname').should('eq', '/account/signin');
 
-    cy.get('input#email').type('ajithpmohan@example.com');
-    cy.get('input#firstName').type('Ajith');
-    cy.get('input#lastName').type('P Mohan');
+    cy.get('input#email').type('rider@example.com');
     cy.get('input#password').type('abc12345', { log: false });
-    cy.get('input#password2').type('abc12345', { log: false });
-    cy.get('select#groups').select('RIDER');
-    cy.get('button').contains('Sign Up').click();
+    cy.get('button').contains('Sign In').click();
 
-    cy.location('pathname').should('eq', '/');
+    cy.location('pathname').should('eq', '/rider');
+
+    cy.visit('/account/signin');
+    cy.location('pathname').should('eq', '/rider');
   });
 });
