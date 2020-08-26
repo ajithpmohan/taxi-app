9e387f4695a7dad215bcb94ede4dab557c7f1005
Ajith P Mohan <ajithpmohan90@gmail.com>
2020-08-26 22:23:32 +0530

    refactor: authentication e2e via cypress modified

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -2,170 +2,110 @@
 import 'cypress-file-upload';
 
 describe('Authentication', () => {
-  it('Can sign up', () => {
-    // stub API call
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: 'http://0.0.0.0:9000/api/v1/account/sign-up/',
-      response: {
-        message:
-          'Account activation email sent to ajithpmohan@example.com',
-      },
-    });
 
+  it('Can sign up as driver', () => {
     cy.visit('/account/sign-up');
 
-    cy.fixture('images/rider.png').then((avatar) => {
+/*    cy.fixture('images/rider.png').then((avatar) => {
       cy.get('input#avatar').attachFile({
         fileContent: avatar,
         fileName: 'avatar.jpg',
         mimeType: 'application/json',
       });
-    });
+    });*/
+
     cy.get('input#email').type('ajithpmohan@example.com');
     cy.get('input#firstName').type('Ajith');
     cy.get('input#lastName').type('P Mohan');
     cy.get('input#password').type('abc12345', { log: false });
     cy.get('input#password2').type('abc12345', { log: false });
+    cy.get('select#groups').select('DRIVER');
+    cy.get('button').contains('Sign Up').click();
+
+    cy.location('pathname').should('eq', '/');
+  });
+
+
+  it('Can sign up as rider', () => {
+    cy.visit('/account/sign-up');
+
+/*    cy.fixture('images/rider.png').then((avatar) => {
+      cy.get('input#avatar').attachFile({
+        fileContent: avatar,
+        fileName: 'avatar.jpg',
+        mimeType: 'application/json',
+      });
+    });*/
+
+    cy.get('input#email').type('regipmohan@example.com');
+    cy.get('input#firstName').type('Regi');
+    cy.get('input#lastName').type('P Mohan');
+    cy.get('input#password').type('abc12345', { log: false });
+    cy.get('input#password2').type('abc12345', { log: false });
     cy.get('select#groups').select('RIDER');
     cy.get('button').contains('Sign Up').click();
-    cy.location('pathname').should('eq', '/');
-  });
-
-  it('Can log in as admin', () => {
-    // stub API call
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: 'http://0.0.0.0:9000/api/v1/account/token/',
-      response: {
-        refresh: 'REFRESH_TOKEN',
-        access: 'ACCESS_TOKEN',
-        user: {
-          email: 'admin@example.com',
-          fullname: 'Admin',
-          avatar: null,
-          type: 'ADMIN',
-        },
-      },
-    });
-
-    cy.visit('/account/sign-in');
-
-    cy.get('input#email').type('admin@example.com');
-    cy.get('input#password').type('abc12345', { log: false });
-    cy.get('button')
-      .contains('Sign In')
-      .click()
-      .should(() => {
-        expect(
-          JSON.parse(localStorage.getItem('authUser')).user.email,
-        ).to.eq('admin@example.com');
-      });
 
     cy.location('pathname').should('eq', '/');
   });
+
 
   it('Can log in as driver', () => {
-    // stub API call
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: 'http://0.0.0.0:9000/api/v1/account/token/',
-      response: {
-        refresh: 'REFRESH_TOKEN',
-        access: 'ACCESS_TOKEN',
-        user: {
-          email: 'driver@example.com',
-          fullname: 'Driver',
-          avatar: null,
-          type: 'DRIVER',
-        },
-      },
-    });
-
     cy.visit('/account/sign-in');
 
-    cy.get('input#email').type('driver@example.com');
+    cy.get('input#email').type('ajithpmohan@example.com');
     cy.get('input#password').type('abc12345', { log: false });
     cy.get('button')
       .contains('Sign In')
       .click()
       .should(() => {
-        expect(
-          JSON.parse(localStorage.getItem('authUser')).user.email,
-        ).to.eq('driver@example.com');
+        const authUser = JSON.parse(localStorage.getItem('authUser'));
+        expect(authUser.user.email).to.eq('ajithpmohan@example.com');
+        expect(authUser.user.fullname).to.eq('Ajith P Mohan');
+        expect(authUser.user.type).to.eq('DRIVER');
       });
 
+    cy.visit('/account/sign-in');
     cy.location('pathname').should('eq', '/driver');
   });
 
-  it('Can log in as rider', () => {
-    // stub API call
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: 'http://0.0.0.0:9000/api/v1/account/token/',
-      response: {
-        refresh: 'REFRESH_TOKEN',
-        access: 'ACCESS_TOKEN',
-        user: {
-          email: 'rider@example.com',
-          fullname: 'Rider',
-          avatar: null,
-          type: 'RIDER',
-        },
-      },
-    });
 
+  it('Can log in as rider', () => {
     cy.visit('/account/sign-in');
 
-    cy.get('input#email').type('rider@example.com');
+    cy.get('input#email').type('regipmohan@example.com');
     cy.get('input#password').type('abc12345', { log: false });
     cy.get('button')
       .contains('Sign In')
       .click()
       .should(() => {
-        expect(
-          JSON.parse(localStorage.getItem('authUser')).user.email,
-        ).to.eq('rider@example.com');
+        const authUser = JSON.parse(localStorage.getItem('authUser'));
+        expect(authUser.user.email).to.eq('regipmohan@example.com');
+        expect(authUser.user.fullname).to.eq('Regi P Mohan');
+        expect(authUser.user.type).to.eq('RIDER');
       });
 
+    cy.visit('/account/sign-in');
     cy.location('pathname').should('eq', '/rider');
   });
+
+
   it('Can log out', () => {
-    // stub API call
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: 'http://0.0.0.0:9000/api/v1/account/token/',
-      response: {
-        refresh: 'REFRESH_TOKEN',
-        access: 'ACCESS_TOKEN',
-        user: {
-          email: 'admin@example.com',
-          fullname: 'Admin',
-          avatar: null,
-          type: 'ADMIN',
-        },
-      },
-    });
 
     cy.visit('/account/sign-in');
 
-    cy.get('input#email').type('admin@example.com');
+    cy.get('input#email').type('ajithpmohan@example.com');
     cy.get('input#password').type('abc12345', { log: false });
     cy.get('button')
       .contains('Sign In')
       .click()
       .should(() => {
-        expect(
-          JSON.parse(localStorage.getItem('authUser')).user.email,
-        ).to.eq('admin@example.com');
+        const authUser = JSON.parse(localStorage.getItem('authUser'));
+        expect(authUser.user.email).to.eq('ajithpmohan@example.com');
+        expect(authUser.user.type).to.eq('DRIVER');
       });
 
-    cy.location('pathname').should('eq', '/');
+    cy.visit('/account/sign-in');
+    cy.location('pathname').should('eq', '/driver');
 
     cy.get('button#navbarDropdownMenuLink').click();
     cy.get('button')
@@ -174,38 +114,25 @@ describe('Authentication', () => {
       .should(() => {
         expect(localStorage.getItem('authUser')).to.be.null;
       });
+
+    cy.location('pathname').should('eq', '/');
   });
 
+
   it('Cannot visit the login or signup pages when logged in', () => {
-    // stub API call
-    cy.server();
-    cy.route({
-      method: 'POST',
-      url: 'http://0.0.0.0:9000/api/v1/account/token/',
-      response: {
-        refresh: 'REFRESH_TOKEN',
-        access: 'ACCESS_TOKEN',
-        user: {
-          email: 'rider@example.com',
-          fullname: 'Rider',
-          avatar: null,
-          type: 'RIDER',
-        },
-      },
-    });
 
     cy.visit('/account/sign-in');
     cy.location('pathname').should('eq', '/account/sign-in');
 
-    cy.get('input#email').type('rider@example.com');
+    cy.get('input#email').type('regipmohan@example.com');
     cy.get('input#password').type('abc12345', { log: false });
     cy.get('button')
       .contains('Sign In')
       .click()
       .should(() => {
-        expect(
-          JSON.parse(localStorage.getItem('authUser')).user.email,
-        ).to.eq('rider@example.com');
+        const authUser = JSON.parse(localStorage.getItem('authUser'));
+        expect(authUser.user.email).to.eq('regipmohan@example.com');
+        expect(authUser.user.type).to.eq('RIDER');
       });
 
     cy.location('pathname').should('eq', '/rider');
